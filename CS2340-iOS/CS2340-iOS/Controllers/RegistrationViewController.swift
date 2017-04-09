44e200099c547bb58360bbca18abc83d12cc3a5a
cliffpanos <cliffpanos@gmail.com>
2017-04-08 23:38:51 -0400

    Changed RegistrationViewController

--- CS2340-iOS/CS2340-iOS/Controllers/RegistrationViewController.swift
+++ CS2340-iOS/CS2340-iOS/Controllers/RegistrationViewController.swift
@@ -26,43 +26,48 @@ class RegistrationViewController: UIViewController, UITextFieldDelegate {
         if let username = usernameText?.text, let password = passwordText?.text, let confirmPassword = confirmPasswordText?.text {
             if username.characters.count == 0 {
                 
+                badRegistrationFeedback(forError: "Enter a username")
                 return
             }
             if password.characters.count == 0 {
                 
+                badRegistrationFeedback(forError: "Enter a password")
                 return
             }
             if confirmPassword.characters.count == 0 {
-                
+                badRegistrationFeedback(forError: "Confirm your password")
                 return
             }
             if confirmPassword != password {
                 
+                badRegistrationFeedback(forError: "Passwords must match")
                 return
             }
             
             AuthManager.shared.registerStandard(username: username, password: password) {
                 (isSuccessful) -> Void in
                 if (isSuccessful) {
+                    self.errorMessageLabel.text = ""
                     self.performSegue(withIdentifier: "registrationToProfile", sender: nil)
                 } else {
                     print("Registration Unsuccessful")
                 }
             }
         }
-        
-        func badLoginFeedback() {
-            self.errorMessageLabel.text = "Invalid Login"
-            let animation = CABasicAnimation(keyPath: "transform.translation.x")
-            animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
-            animation.repeatCount = 3
-            animation.duration = 0.07
-            animation.autoreverses = true
-            animation.byValue = 7
-            self.usernameText.layer.add(animation, forKey: "position")
-            self.passwordText.layer.add(animation, forKey: "position")
-            self.confirmPasswordText.layer.add(animation, forKey: "position")
-        }
+    }
+    
+    func badRegistrationFeedback(forError errorMessage: String) {
+
+        self.errorMessageLabel.text = errorMessage
+        let animation = CABasicAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.repeatCount = 3
+        animation.duration = 0.07
+        animation.autoreverses = true
+        animation.byValue = 7
+        self.usernameText.layer.add(animation, forKey: "position")
+        self.passwordText.layer.add(animation, forKey: "position")
+        self.confirmPasswordText.layer.add(animation, forKey: "position")
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
